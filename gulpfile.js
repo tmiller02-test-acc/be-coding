d90ceb8a60c98f883aa8d0d2890661dc4dc44004
Tom Miller <02millert@gmail.com>
2015-10-04 21:02:31 +0100

    Watch for scss changes in default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -42,5 +42,9 @@ gulp.task('copy-js', function () {
 	gulp.src(baseSrcDir + '/components/modernizr/modernizr.js').pipe(gulp.dest(baseDestDir + '/js/'));
 });
 
+gulp.task('watch-styles', function() {
+	gulp.watch(baseSrcDir + '/scss/*.scss', ['build-styles']);
+});
+
 gulp.task('build', ['build-styles', 'copy-styles', 'copy-js', 'concat-js'])
-gulp.task('default', ['build-styles']);
+gulp.task('default', ['build-styles', 'watch-styles']);
